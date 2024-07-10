581f975c12ee7d7212397d9520c7c57e477a8b7f
Avazbek <saidaliyev.avazbek14@gmail.com>
2024-07-10 22:58:40 +0500

    catalog header dev finished

--- src/components/CatalogHeader.vue
+++ src/components/CatalogHeader.vue
@@ -3,6 +3,10 @@
     <div class="container">
         <div class="row">
             <h2 class="catalog__header-title all-title">{{ store.title }}</h2>
+
+            <div class="catalog__header-images">
+                <img :src="img" alt="" :class="`catalog__header-img img-${idx + 1}`" v-for="(img, idx) in store.images" :key="idx">
+            </div>
         </div>
     </div>
   </header>
@@ -15,7 +19,7 @@ export default {
     name: 'Catalog Header',
     data() {
         return {
-            store: cHeaderStore()
+            store: cHeaderStore(),
         }
     }
 }
@@ -28,12 +32,37 @@ export default {
     width: 100%;
     padding-top: 20px;
 
+    .row {
+        flex-direction: column;
+        row-gap: 20px;
+    }
+
     &-title {
         font-weight: 300;
         background: -webkit-linear-gradient(#333,  #eee);
         -webkit-background-clip: text;
         -webkit-text-fill-color: transparent;
     }
+
+    &-images {
+        width: 100%;
+        display: grid;
+        grid-template-columns: repeat(4, 1fr);
+        grid-template-rows: repeat(4, 1fr);
+        grid-column-gap: 0px;
+        grid-row-gap: 0px;
+    }
+
+    &-img {
+        width: 100%;
+        height: 100%;
+        object-fit: cover;
+
+        &.img-1 { grid-area: 1 / 1 / 5 / 3; }
+        &.img-2 { grid-area: 1 / 3 / 3 / 5; }
+        &.img-3 { grid-area: 3 / 3 / 5 / 5; }
+    }
+
 }
 
 </style>
